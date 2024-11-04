5d399051d3b915a48de26b4c376898f06752090e
OMAR-DIB <omardib759@gmail.com>
2024-11-04 21:00:08 +0200

    ignore test

---
+++ webpack-practice/src/index.js
@@ -0,0 +1,9 @@
+import "./style.css";
+import odinImage from "./assets/odin-lined.png";
+import { greeting } from "./greeting.js";
+
+const image = document.createElement("img");
+image.src = odinImage;
+   
+document.body.appendChild(image);
+console.log(greeting);
