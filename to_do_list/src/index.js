2df08d1538249ef36a4d4fdd0e0b9cec39a976a9
OMAR-DIB <omardib759@gmail.com>
2024-11-10 22:15:42 +0200

    add correct mark

--- to_do_list/src/index.js
+++ to_do_list/src/index.js
@@ -2,7 +2,7 @@ import "./styles.css";
 
 const add = document.querySelector("button");
 const myUL = document.querySelector(".my-list");
-const myfield = document.getElementsByTagName("li");
+const myfield = document.getElementsByTagName("p");
 for (let i = 0; i < myfield.length; i++) {
     const mybutton = document.createElement("span");
     mybutton.textContent = "X";
@@ -11,33 +11,39 @@ for (let i = 0; i < myfield.length; i++) {
 
 add.addEventListener("click", () => {
     event.preventDefault();
-    const field = document.createElement("li");
+    const fieldContainer = document.createElement("div");
+    fieldContainer.className = "field-container";
+
+    const field = document.createElement("p");
     const input = document.querySelector(".myInput").value;
     const text = document.createTextNode(input);
     field.appendChild(text);
-    myUL.appendChild(field);
+    fieldContainer.appendChild(field);
+    myUL.appendChild(fieldContainer);
     document.querySelector(".myInput").value = "";
 
     const mybutton = document.createElement("span");
     mybutton.textContent = "X";
-    field.appendChild(mybutton);
+    fieldContainer.appendChild(mybutton);
     mybutton.style.cursor = "pointer";
     mybutton.onclick = function () {
-        field.remove();
+        fieldContainer.remove();
     }
     field.onclick = function () {
-        if (this.style.color === "red"){
-            this.style.color = "black";
-        }else{
-            this.style.color = "red";
+        if (this.style.textDecoration === "line-through") {
+            this.style.textDecoration = "none";
+        } else {
+            this.style.textDecoration = "line-through";
         }
     };
+    fieldContainer.style.display = "flex";  
+    fieldContainer.style.alignItems = "center";  
 });
 
 // const myfield = document.getElementsByTagName("li");
 
 for (let i = 0; i < myfield.length; i++) {
-  myfield[i].onclick = function () {
-    this.style.color = "red"; // Apply red color to the clicked item
-  };
+    myfield[i].onclick = function () {
+        this.style.color = "red"; // Apply red color to the clicked item
+    };
 }
